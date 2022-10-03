d057959e0f8bd9c99add2a514c8dd12d7e4e9f9a
Flávio Bulhões <flavio.profissional1@gmail.com>
2022-10-03 18:04:14 -0300

    botão de logout

--- src/routes/Posts/Posts.jsx
+++ src/routes/Posts/Posts.jsx
@@ -1,12 +1,17 @@
 import style from "./Posts.css";
 import { NewPost } from "../../components/NewPost";
 import { Post } from "../../components/Post";
+import { FiLogOut } from "react-icons/fi";
+import { Outlet, Link } from "react-router-dom";
 
 export function Posts() {
   return (
     <div className="Posts">
       <header>
         <h3>CodeLeap Network</h3>
+        <Link to={`/`}>
+          <FiLogOut className="logoutbtn" />
+        </Link>
       </header>
       <section>
         <NewPost />
